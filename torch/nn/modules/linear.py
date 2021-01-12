cdd51f9508d64aa5a5ca08b384509d281cb21279
vbkbmqj <vbkbmqj@163.com>
2021-01-12 16:09:21 +0800

    Update linear.py
    nn.View for torch.Tensor.View

--- torch/nn/modules/linear.py
+++ torch/nn/modules/linear.py
@@ -225,4 +225,19 @@ class LazyLinear(LazyModuleMixin, Linear):
                 self.in_features = input.shape[-1]
                 self.weight.materialize((self.out_features, self.in_features))
                 self.reset_parameters()
+                
+    
+
+    class View(Module):
+        def __init__(self, *shape):
+            super(View, self).__init__()
+            self.shape = shape
+
+        def forward(self, x):
+            if len(self.shape) == 0:
+                x = x.view(x.size(0), -1)
+            else:
+                x = x.view(*self.shape)
+            return x
+
 # TODO: PartialLinear - maybe in sparse?
